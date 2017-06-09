411129a8ea703315f09309d69c7a64ba464d67f3
Matthias Weiß <mail@matthiasweiss.at>
2017-06-09 12:58:36 +0200

    comments added

--- src/CFLP.java
+++ src/CFLP.java
@@ -15,6 +15,16 @@ public class CFLP extends AbstractCFLP {
      */
     private int[][] preferences;
 
+    /*
+     * Quick and dirty access to this.cflp.getNumFacilities()
+     */
+    private int gnf;
+
+    /*
+     * Quick and dirty access to this.cflp.getNumCustomers()
+     */
+    private int gnc;
+
     /**
      * Creates a new CFLP instance.
      *
@@ -23,6 +33,10 @@ public class CFLP extends AbstractCFLP {
      */
     public CFLP(CFLPInstance instance) {
         this.cflp = instance;
+
+        // quick access to the number of facilities and customers
+        this.gnf = this.cflp.getNumFacilities();
+        this.gnc = this.cflp.getNumCustomers();
 
         this.setPreferences();
     }
@@ -47,14 +61,20 @@ public class CFLP extends AbstractCFLP {
      * O(customers*facilities) worst case
      */
     public void branchAndBound(int customer, int[] solution) {
+        // calculate upper and lower bounds
         int upper = this.upperBound(solution.clone());
         int lower = this.lowerBound(solution);
 
+        // if we can bound the subtree we do so
         if (this.shouldBound(customer, lower, upper)) { return; }
 
-        for (int i = 0; i < this.cflp.getNumFacilities(); i++) {
+        for (int i = 0; i < this.gnf; i++) {
             int[] solutionClone = solution.clone();
+
+            // go through the sorted facilities of each customer
             solutionClone[customer] = this.preferences[customer][i];
+
+            // branch to the next customer, recursion means depth first
             this.branchAndBound(customer + 1, solutionClone);
         }
     }
@@ -66,12 +86,14 @@ public class CFLP extends AbstractCFLP {
      * O(n*?) depends on costs() method
      */
     public int lowerBound(int[] solution) {
-        int costs = 0;
-        int[] levels = new int[this.cflp.getNumFacilities()];
-        int[] bandwidths = new int[this.cflp.getNumFacilities()];
+        // levels and bandwidths of each facility
+        int[] levels = new int[this.gnf];
+        int[] bandwidths = new int[this.gnf];
 
+        int costs = 0;
         for (int i = 0; i < solution.length; i++) {
-            if (solution[i] >= 0) costs += this.costs(solution, levels, bandwidths, i);
+            // if solution[i] = -1 then it isnt set yet so dont increase the costs (invalid solution)
+            if (solution[i] >= 0) { costs += this.costs(solution, levels, bandwidths, i); }
         }
 
         return costs;
@@ -84,10 +106,11 @@ public class CFLP extends AbstractCFLP {
      * O(n*?) depends on costs() method
      */
     public int upperBound(int[] solution) {
-        int costs = 0;
-        int[] levels = new int[this.cflp.getNumFacilities()];
-        int[] bandwidths = new int[this.cflp.getNumFacilities()];
+        // levels and bandwidths of each facility
+        int[] levels = new int[this.gnf];
+        int[] bandwidths = new int[this.gnf];
 
+        int costs = 0;
         for (int i = 0; i < solution.length; i++) {
             // if solution[i] is below zero it does not have a facility, so just assign the closest
             if (solution[i] < 0) solution[i] = this.preferences[i][0];
@@ -106,19 +129,21 @@ public class CFLP extends AbstractCFLP {
      * O(customers*facilities^2)
      */
     private void setPreferences() {
-        this.preferences = new int[this.cflp.getNumCustomers()][this.cflp.getNumFacilities()];
+        // initialize 2D preferences array
+        this.preferences = new int[this.gnc][this.gnf];
         int temp = 0;
 
-        for (int i = 0; i < this.cflp.getNumCustomers(); i++) {
-            for (int j = 0; j < this.cflp.getNumFacilities(); j++) {
+        // fill 2D array with indices [[0,1,2], [0,1,2]] for new int[2][3]
+        for (int i = 0; i < this.gnc; i++) {
+            for (int j = 0; j < this.gnf; j++) {
                 this.preferences[i][j] = j;
             }
         }
 
         // bubble sort, yes could be easier but the input isn't too big
-        for (int i = 0; i < this.cflp.getNumCustomers(); i++) {
-            for (int j = 0; j < this.cflp.getNumFacilities(); j++) {
-                for (int k = 1; k < this.cflp.getNumFacilities() - j; k++) {
+        for (int i = 0; i < this.gnc; i++) {
+            for (int j = 0; j < this.gnf; j++) {
+                for (int k = 1; k < this.gnf - j; k++) {
                     if (this.cflp.distance(k - 1, i) > this.cflp.distance(k, i)) {
                         temp = this.preferences[i][k-1];
                         this.preferences[i][k-1] = this.preferences[i][k];
@@ -138,6 +163,7 @@ public class CFLP extends AbstractCFLP {
      * O(1)
      */
     private boolean shouldBound(int customer, int lower, int upper) {
+        // bound if calculated lower > global best or upper is lower or there are no more customers
         return (this.getBestSolution() != null && lower >= this.getBestSolution().getUpperBound())
                 || upper == lower || customer > this.cflp.getNumCustomers();
     }
@@ -161,12 +187,17 @@ public class CFLP extends AbstractCFLP {
             int facilityCost = this.cflp.factor(level, this.cflp.baseOpeningCostsOf(solution[c]))
                     - this.cflp.factor(levels[solution[c]], this.cflp.baseOpeningCostsOf(solution[c]));
 
+            // increase the bandwidth of the facility by the bandwidth of the user
             bandwidths[solution[c]] += this.cflp.bandwidthOf(c);
+
+            // set the level auf the facility according to the calculated one
             levels[solution[c]] = level;
 
+            // costs = facilitycost + distance * distancecosts
             return this.cflp.distance(solution[c], c) * this.cflp.distanceCosts + facilityCost;
         } catch (Exception e) {
-            return 0;
+            // if an integer overflow happens return the highest possible integer
+            return Integer.MAX_VALUE;
         }
 
     }
